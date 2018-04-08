3aa5f39f8e74da5c0d426db7d0bbe060917f01be
EOussama <oussama_gtacj@live.fr>
2018-04-08 16:18:37 +0100

    Initial build

--- EO Browser/Form1.Designer.vb
+++ EO Browser/Form1.Designer.vb
@@ -22,14 +22,17 @@ Partial Class fMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(fMain))
         Me.SuspendLayout()
         '
         'fMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(948, 466)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "fMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "EO Browser"
         Me.ResumeLayout(False)
 
